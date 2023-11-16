940448f9a85ceae1388c088d2671b1e42948a322
Rachel Hamby <tnthreat33@gmail.com>
2023-11-15 19:51:41 -0500

    done

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,9 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+
+    session.add_all([botw, ffvii, mk8])
+    session.commit()  
